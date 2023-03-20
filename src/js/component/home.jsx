4583ace32e213abaf31b2392e9e49f7384c97ee0
LesCamposR <lesthercamposramirez91@gmail.com>
2023-03-20 18:57:40 +0000

    First

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+//Import my CSS files
+import ToDoList from "../../styles/ToDoList.css"
 
 //create your first component
+
 const Home = () => {
+	/*const arrTemp = ["Read", "WorkOut", "Surf"]*/
+	const [arrTemp, setarrTemp] = useState (["Read", "WorkOut", "Surf"])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center mt-5">
+			<input placeholder="Add a new task"
+			onKeyDown={(e)=> {
+				if(e.keyCode=="13"){
+					console.log("Enter")
+				}
+			}}>
+			</input>
+			<div className="row">
+				<h2 className="text-success">ToDos List :</h2>
+			</div>
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-3">
+					{arrTemp && arrTemp.length > 0 ?
+					
+					<>{arrTemp.map((item, index) => {
+						
+						return<li key={index} className=" d-flex justify-content-between"  >
+							{item} <button className="ocultar btn m-1" type="button"> <i class="fa-solid fa-x fa-xs  "></i></button>
+
+						</li>
+					})}</>
+					:
+					<><h4>There are not Tasks to Do</h4></>}
+					</div>
+	
+
+				</div>
+			
+			</div>
 		</div>
 	);
 };
