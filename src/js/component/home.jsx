954b9a96695a798afbc89ffb63d5b4b53e503fc6
LesCamposR <lesthercamposramirez91@gmail.com>
2023-03-22 19:21:25 +0000

    Blessings

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -22,33 +22,37 @@ const Home = () => {
 	useEffect (()=>{console.log("Se renderizo")},[arrTemp])
 	return (
 		<div className="text-center mt-5">
-			<input placeholder="Add a new task"
+			<input  placeholder="Add a new task"
 				onKeyDown={(e) => {
 					if (e.keyCode == "13") {
 						console.log("Enter")
 						setarrTemp([...arrTemp, {tarea: e.target.value, done:false}])
+						e.target.value = "";
 					}
 				}}>
 			</input>
 			<div className="row">
 				<h2 className="text-success">ToDos List :</h2>
 			</div>
-			<div className="container">
+			<div className="container" >
 				<div className="row justify-content-center">
-					<div className="col-3">
+					<div className="col-4 border bg-white">
 						{arrTemp && arrTemp.length > 0 ?
 
 							<>{arrTemp.map((item, index) => {
 
-								return <li key={index} className=" d-flex justify-content-between">
-									{item.tarea} - {item.done? "Completed" : "Pending"}
+								return <li key={index} className=" d-flex justify-content-between list-group-item">
+									{item.tarea}      -Status:  {item.done? "Completed" : "In Progress"}
 									<button onClick={() => {
 										eliminarTarea(index)
-									}} className="ocultar btn m-1" type="button"> <i class="fa-solid fa-x fa-xs  "></i>
+									}} className="ocultar btn m-1" type="button"> <i class="fa-solid fa-x fa-xs pt-1 justify-content-end "></i>
 									</button>
 
 								</li>
-							})}</>
+								
+							})}
+							<span className="font-weight-light m-2">{arrTemp.length} Number of Tasks</span>
+							</>
 							:
 							<><h4>There are not Tasks to Do</h4></>}
 					</div>
